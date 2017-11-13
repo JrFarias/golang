100ecdee35cb209524f329eb857e1f32ddd29134
junior <junior@beetech.global>
2017-11-13 17:20:11 -0200

    feat(monitoring): finishing service

--- prod_hello/hello.go
+++ prod_hello/hello.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -17,7 +22,7 @@ func main() {
 		case 1:
 			initMonitoring()
 		case 2:
-			fmt.Println("Exibindo Logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do programa...")
 			os.Exit(0)
@@ -52,7 +57,7 @@ func initMonitoring() {
 	fmt.Println("Monitorando...")
 	const monitoramento = 5
 	const delay = 5
-	sites := []string{"https://www.alura.com.br", "https://www.google.com.br"}
+	sites := readFile()
 	for i := 0; i < monitoramento; i++ {
 		for _, site := range sites {
 			testSite(site)
@@ -63,11 +68,65 @@ func initMonitoring() {
 }
 
 func testSite(site string) {
-	resp, _ := http.Get(site)
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro", err)
+	}
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site", site, "foi carregado com sucesso", resp.StatusCode)
+		saveLog(site, true)
 	} else {
 		fmt.Println("Site", site, "esta com problemas", resp.StatusCode)
+		saveLog(site, false)
 	}
+}
+
+func readFile() []string {
+	var sites []string
+
+	file, err := os.Open("sites.txt")
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, err := reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+
+		sites = append(sites, line)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	file.Close()
+
+	return sites
+}
+
+func saveLog(site string, status bool) {
+
+	file, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Println("ocorreu um erro", err)
+	}
+
+	file.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	fmt.Println(site, status, file)
+	file.Close()
+}
+
+func imprimeLogs() {
+	fmt.Println("Exibindo Logs...")
+	file, err := ioutil.ReadFile("log.txt")
+	if err != nil {
+		fmt.Println("ocorreu um erro", err)
+	}
+
+	fmt.Println(string(file))
 }
