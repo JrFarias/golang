cfd653631d748b634f30ce12e7019785f080ccf2
junior <junior@beetech.global>
2017-11-22 09:54:23 -0200

    feat(prod_mvc): add new project MVC

---
+++ prod_mvc/src/github.com/lss/webapp/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"golang/prod_mvc/src/github.com/lss/webapp/controller"
+)
+
+func main() {
+	templates := populateTemplates()
+	controller.Startup(templates)
+	http.ListenAndServe(":8000", nil)
+}
+
+func populateTemplates() map[string]*template.Template {
+	result := make(map[string]*template.Template)
+	const basePath = "src/golang/prod_mvc/templates"
+	fmt.Println(template.ParseFiles(basePath))
+	layout := template.Must(template.ParseFiles(basePath + "/_layout.html"))
+
+	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html"))
+	dir, err := os.Open(basePath + "/content")
+	if err != nil {
+		panic("Failed to open template blocks directory: " + err.Error())
+	}
+	fis, err := dir.Readdir(-1)
+	if err != nil {
+		panic("Failed to read contents of content directory: " + err.Error())
+	}
+	for _, fi := range fis {
+		f, err := os.Open(basePath + "/content/" + fi.Name())
+		if err != nil {
+			panic("Failed to open template '" + fi.Name() + "'")
+		}
+		content, err := ioutil.ReadAll(f)
+		if err != nil {
+			panic("Failed to read content from file '" + fi.Name() + "'")
+		}
+		f.Close()
+		tmpl := template.Must(layout.Clone())
+		_, err = tmpl.Parse(string(content))
+		if err != nil {
+			panic("Failed to parse contents of '" + fi.Name() + "' as template")
+		}
+		result[fi.Name()] = tmpl
+	}
+	return result
+}
